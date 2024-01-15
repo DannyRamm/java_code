1d66d7ecbfebf4e96fd22847859266533fe7c498
DannyRamm <202213866g@7utea.edu.pe>
2024-01-15 10:43:40 -0500

    primer commit

--- CaracteresEspeciales.java
+++ CaracteresEspeciales.java
@@ -1,10 +1,7 @@
 
 public class CaracteresEspeciales {
     public static void main(String[] args) {
-        // Ejemplo de cadena con caracteres especiales
-        String cadenaConEspeciales = "¡Hola, mundo! Este es un carácter especial: \u00A9";
-
-        // Imprimir la cadena
-        System.out.println(cadenaConEspeciales);
+       System.out.println("una corchea: \u266A");    
+       System.out.print("Dos corcheas: \u266B");
     }
 }
